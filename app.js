6810727f1d785893d5a262d5d08377b7eb28fe9f
Amr <bawzeeramr@gmail.com>
2024-01-17 21:25:38 +0000

    Create custom input for grid size

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-// create 16x16 grid of div using javascript
-// put grid of divs inside another div (holds your grids)
-// use flexbox to make divs appear as grid
 // user hover effect to change each div whenever the mouse goes over it
-    // you can change color by either adding a class or changiing backgroundColor of the divs
+    // you can change color by either adding a class or changing backgroundColor of the divs
 // add button to the top of screen that will prompt user to change number of divs per side
 // max input is 100 squares
 
+const myForm = document.querySelector('#myForm');
+const input = document.querySelector('#sizeInput');
 
-// currently trying to create custom grid size with an input field and a button
-
-
-
-const input = document.querySelector('input');
+myForm.addEventListener('submit', e => {
+e.preventDefault();
+if (input.value>100 || input.value==0) {
+    alert("That size will break everything")
+} else {
+    resetGrid();
+    makeGrid(input.value)
+}
+});
 
 function makeGrid(e) {
+
     const parentGrid = document.querySelector('#parentGrid');
 
         for (let i = 0; i<e; i++) {
@@ -33,27 +37,13 @@ function makeGrid(e) {
         }
 }
 
-function defaultGrid() {
-    const parentGrid = document.querySelector('#parentGrid');
 
-        for (let i = 0; i<16; i++) {
+function resetGrid() {
 
-            const row = document.createElement('div');
-            row.id = 'row' + i;
-            row.setAttribute('class', 'rowContainer');
-            parentGrid.append(row);
+    while (parentGrid.firstChild) {
+        parentGrid.removeChild(parentGrid.firstChild);
+    }
 
-            const rowWidth = document.getElementById('row' + i)
-
-            for (let x = 0; x<16; x++) {
-                const tile = document.createElement('div');
-                tile.setAttribute('class', 'tiles');
-                rowWidth.append(tile);
-            }
-        }
 }
 
-function resetGrid() {}
-
-defaultGrid();
-input.addEventListener('input', makeGrid);
+makeGrid(16);
